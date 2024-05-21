3123633c6b7fed0dee27b0e6db790425bac16565
jorge vergara <jorgevergarasou@gmail.com>
2024-05-21 14:49:00 -0300

    archivo env creado

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,30 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PetsModule } from './pets/pets.module';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+import {DB_TYPE, HOST, USER_DB_NAME, USER_DB_PASSWORD, PORT, DATABASE_NAME   } from '../config.js'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env'], // Puedes incluir otros archivos .env aquí si es necesario
+      isGlobal: true, // Hace que el módulo de configuración esté disponible en toda la aplicación
+    }),
 // CONFIGURACIONES DE LA CONECCION A LA "BD"
     TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "root",
-      password: "161poker",
-      database: "petlost",
+      type: DB_TYPE,
+      host: HOST,
+      username: USER_DB_NAME,
+      password: USER_DB_PASSWORD,
+      port: PORT,
+      database: DATABASE_NAME,
       autoLoadEntities: true, // CARGA LAS ENTITYS DE FORMA AUTOMATICA PARA NO HACERLO MANUAL
       synchronize: true, // TODO CAMBIO QUE SE GENERE ACA, SE SINCRONIZA CON LA "BD"
+      /** DB_HOST=localhost
+DB_PORT=3306
+DB_USER=root
+DB_PASSWORD=161poker
+DB_NAME=petlost */
     }),    
     BreedsModule, UsersModule, AuthModule, PetsModule, PostsModule],
   controllers: [],
--- src/auth/constants/jwt.constant.ts
+++ src/auth/constants/jwt.constant.ts
@@ -1,3 +1,3 @@
 export const jwtConstants = {
-    secret: "no usar esta palabra en produccion"
+    secret: 'Super_$_S3cr3t_#_Key_@_l0tr',
 }; /** sirve para verificar que el token que se ha creado coincide con el que se esta enviando */
